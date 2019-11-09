e5e500d10f3bd88f38d3e24dba5d35dbbeb3172c
Cole Christini <cole.christini@gmail.com>
2019-11-09 00:16:17 -0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OW_Game_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
