5803bb17eced58c456be44beb1a327590d60802e
Cole Christini <cole.christini@gmail.com>
2019-12-02 12:21:23 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ describe('#extractFrames()', async function() {
 		it('should return one kill with storm arrow headshot', async function() {
 
 			// use await to wait until the promise is fulfilled
-			let data = await extractFrames("../resources/test/test_1.mp4", false) .catch(error => console.log(error));
+			let data = await extractFrames("../resources/test/test_1.mp4", false).catch((e) => { console.error(e.message) })
 
 			// add some assertions
 			expect(data)
@@ -17,13 +17,12 @@ describe('#extractFrames()', async function() {
 
 	})
 
-	context('with non-string argument', function() {
+	context('2 kills occur, melee&ult, spectator', function() {
 		context('tracer gets melee kill on hanzo, mercy resurrects, tracer pulse bombs hanzo, is spectator', function() {
 			it('should return one kill with melee, a resurrection, and tracer pulse bombing hanzo', async function() {
 
 				// use await to wait until the promise is fulfilled
-				let data = await extractFrames("../resources/test/test_2.mp4", true) .catch(error => console.log(error));
-
+				let data = await extractFrames("../resources/test/test_2.mp4", true).catch((e) => { console.error(e.message) })
 				// add some assertions
 				expect(data)
 					.to.be.a('array').and.to.have.members(['NOVA[Hanzo][Blue][Melee][headshot:false][ult:false]->BIPOLAR[Hanzo]', 'KIWI[Mercy][Red[Resurrection][headshot:false][ult:false]->BIPOLAR[Hanzo][Red]', 'NOVA[Hanzo][Blue][Dragonstrike][headshot:false][ult:true]->BIPOLAR[Hanzo][Red]']);
